f023c8d8e1956976c9afc384b36d5225283551e1
Yasmin Heinze <yasminoliveiraheinze@hotmail.com>
2022-07-27 21:03:28 -0300

    Alterando o arquivo python agora sim

--- Day1/Task3/inicio.py
+++ Day1/Task3/inicio.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-
 def inicio():
   print("Iniciando a execucao")
   print("Vou continuar")
   hello()
 
 def hello():
-  valores = ["sol""chuva","vento","noite","lua"]
+  valores = ["sol","chuva","vento","noite","lua"]
   print("Oi")
   loop(valores)
   wFile('/dev/null')
@@ -16,10 +14,9 @@ def loop(var1):
     print(i)
 
 def wFile(fname):
-  content = 'TWVuc2FnZW0gc2VjcmV0YSwgdm9jZSBjb25zZWd1aXUuIE8gY29kaWdvIMOpIDIwMjIuCg==\n'
+  content = "TWVuc2FnZW0gc2VjcmV0YSwgdm9jZSBjb25zZWd1aXUuIE8gY29kaWdvIMOpIDIwMjIuCg==\n"
   f = open(fname, 'w')
-  file.write(content)
+  f.write(content)
   f.close()
-
 
 inicio()
