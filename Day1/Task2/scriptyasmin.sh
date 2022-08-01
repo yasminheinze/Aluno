81bf917453e8715d06bd2465ad4c5ed6938f3efb
Yasmin Heinze <yasminoliveiraheinze@hotmail.com>
2022-08-01 20:45:32 -0300

    Correcao

--- Day1/Task2/scriptyasmin.sh
+++ Day1/Task2/scriptyasmin.sh
@@ -3,19 +3,23 @@
 echo "Bem-vindo Mentor/Aluno ao Desenvolve 2022"
 
 echo "O Seu IP segundo o site ifconfig.me é ...."
-curl ifconfig.me
-echo
-
-if ! ping -c 3 ifconfig.me ; then
+ip=`curl ifconfig.me`
 
 echo
-echo "Você não pode acessar o site ifconfig.me, este site não responde a ping"
+echo $ip
+echo
+
+
+if ! ping -c 3 $ip ; then
+
+echo
+echo "Este endereço não responde a ping, ou por restrições ou por inatividade"
 echo
 
 else
 
 echo
-echo "Parabéns você pode acessar ifconfig.me este site está respondendo a ping"
+echo "Parabéns este IP  está respondendo a ping"
 echo
 
 fi
